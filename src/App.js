e24198c79ecadadec919045bb43eca4cd3f8eeac
Pedro Britto <pedroivobritto@gmail.com>
2019-05-01 08:23:28 -0300

    Add boilerplate css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from "react";
 
 function App() {
-    return <div className="App" />;
+    return (
+        <div className="App">
+            <div className="padding-vertical--big">
+                <div className="container">
+                    <h2>Hello from Exia</h2>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat quos officia
+                        eaque, temporibus facilis quidem.
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
