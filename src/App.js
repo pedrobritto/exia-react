aac1187bbaa32fd1fa0905eccf4601110b80151d
Pedro Britto <pedroivobritto@gmail.com>
2019-09-17 21:25:05 -0300

    Minor tweaks

--- src/App.js
+++ src/App.js
@@ -3,27 +3,27 @@ import React from "react";
 import PageWrapper from "./components/PageWrapper/PageWrapper";
 import ContentWrapper from "./components/PageWrapper/ContentWrapper";
 import FooterWrapper from "./components/PageWrapper/FooterWrapper";
+import MessageBox from "./components/ui/MessageBox";
 
 function App() {
     return (
-        <div className="App">
-            <PageWrapper>
-                <ContentWrapper>
-                    <div className="padding-vertical--big">
-                        <div className="container">
-                            <h2>Hello from Exia</h2>
-                            <p>
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat quos
-                                officia eaque, temporibus facilis quidem.
-                            </p>
-                        </div>
+        <PageWrapper>
+            <ContentWrapper>
+                <div className="padding-vertical--big">
+                    <div className="container">
+                        <h2>Hello from Exia</h2>
+                        <MessageBox color="red">Lorem ipsum dolor sit amet.</MessageBox>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat quos
+                            officia eaque, temporibus facilis quidem.
+                        </p>
                     </div>
-                </ContentWrapper>
-                <FooterWrapper>
-                    <div className="container">Footer</div>
-                </FooterWrapper>
-            </PageWrapper>
-        </div>
+                </div>
+            </ContentWrapper>
+            <FooterWrapper>
+                <div className="container">Footer</div>
+            </FooterWrapper>
+        </PageWrapper>
     );
 }
 
